576cb351bccdb6451275e33f44eb01535e6e3f69
fangYiTao <718891998@qq.com>
2021-08-12 11:42:06 +0800

    登录用户名和密码的提交

---
+++ js/login.js
@@ -0,0 +1,16 @@
+$(function () {
+
+  $('#login').on('click', function (e) {
+    // 阻止按钮的默认刷新页面
+    e.preventDefault()
+
+    // 获取用户名和密码
+    const username = $('#username').val().trim()
+    const password = $('#password').val().trim()
+
+    // js 打断点
+    // debugger
+
+  })
+
+})
