ecc034817268b1c2f7cf212697562112c9346ea5
fangYiTao <718891998@qq.com>
2021-08-12 16:27:10 +0800

    后台-首页-完成退出

--- js/index.js
+++ js/index.js
@@ -7,10 +7,21 @@ $(function () {
       // 请求头  token 都是放在请求头!!!当众1发送给后台!!!
       headers: { Authorization: localStorage.getItem('token') },
       success(res) {
-        console.log(res)
+        const user = res.data
+        $('.user_info span').text(user.nickname)
+        $('.user_info img').prop('src', user.userPic)
+        $('.user_center_link img').prop('src', user.userPic)
       }
     })
   }
+
+  // 退出功能
+  $('.logout').on('click', function () {
+    if (confirm('确定退出吗')) {
+      localStorage.removeItem('token')
+      location.href = 'login.html'
+    }
+  })
 
 
 
