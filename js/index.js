dd7db537b0f4f1af7e36a12a7124778b7bce1e49
fangYiTao <718891998@qq.com>
2021-08-12 16:15:18 +0800

    后台静态页面搭建完成

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 $(function () {
-
+  getData()
   // 向后台发起请求，获取用户信息
-  $.ajax({
-    url: 'http://localhost:8080/api/v1/admin/user/info',
-    // 请求头  token 都是放在请求头!!!当众1发送给后台!!!
-    headers: { Authorization: localStorage.getItem('token') },
-    success(res) {
-      console.log(res)
-    }
+  function getData() {
+    $.ajax({
+      url: 'http://localhost:8080/api/v1/admin/user/info',
+      // 请求头  token 都是放在请求头!!!当众1发送给后台!!!
+      headers: { Authorization: localStorage.getItem('token') },
+      success(res) {
+        console.log(res)
+      }
+    })
+  }
 
-  })
+
 
 })
