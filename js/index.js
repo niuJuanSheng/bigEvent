69f69046c0338852cd954091c9a228864ec54620
fangYiTao <718891998@qq.com>
2021-08-12 17:05:40 +0800

    点击一级菜单控制二级菜单的显示

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function () {
-  getData()
+  getUserInfo()
   // 向后台发起请求，获取用户信息
-  function getData() {
+  function getUserInfo() {
     $.ajax({
       url: 'http://localhost:8080/api/v1/admin/user/info',
       // 请求头  token 都是放在请求头!!!当众1发送给后台!!!
@@ -21,6 +21,11 @@ $(function () {
       localStorage.removeItem('token')
       location.href = 'login.html'
     }
+  })
+
+  // 点击一级菜单控制二级菜单
+  $('.level01').on('click', function () {
+    $(this).next('.level02').stop().slideToggle()
   })
 
 
