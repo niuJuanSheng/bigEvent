3ef768c9708a432e4c0e6681f0e3143deb54c80c
fangYiTao <718891998@qq.com>
2021-08-14 11:20:45 +0800

    文章新增分类完成

--- js/atricle_category.js
+++ js/atricle_category.js
@@ -1,3 +1,4 @@
+
 $(function () {
 
   // 获取所有的文章信息
@@ -12,7 +13,6 @@ $(function () {
           // 利用模板引擎template来进行数据的渲染
           let html = template('newListTemp', res)
           $('#tbody').html(html)
-          console.log(res)
         } else {
           console.log('获取失败')
         }
@@ -21,5 +21,41 @@ $(function () {
   }
   getCategoryList()
 
+  // 文章列表新增分类
+  $('.btn_opt').on('click', function (e) {
+    // 阻止默认事件
+    e.preventDefault
+    // 获取输入的数据
+    let name = $('.categoryName').val().trim()
+    let slug = $('.categoryAlias').val().trim()
 
+    if (!name || !slug) {
+      alert('请输入分类名')
+      return
+    }
+    // 发起post请求
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:8080/api/v1/admin/category/add',
+      headers: { Authorization: localStorage.getItem('token') },
+      data: { name, slug },
+      success: function (result) {
+        if (result.code === 200) {
+          // 清空输入框
+          $('.categoryName').val("")
+          $('.categoryAlias').val("")
+          // 关闭模态框
+          $("#myModal").modal('hide')
+          getCategoryList()
+        } else {
+          console.log('请求失败')
+        }
+      }
+    })
+
+
+  })
+
+
+  // 
 })
