7969e1f56cd0733a6f821d8de50ecd9aff8ac4ef
fangYiTao <718891998@qq.com>
2021-08-14 09:54:27 +0800

    文章列表数据的渲染`完成

---
+++ js/atricle_category.js
@@ -0,0 +1,24 @@
+$(function () {
+
+  // 获取所有的文章信息
+  function getCategoryList() {
+    $.ajax({
+      url: 'http://localhost:8080/api/v1/admin/category/list',
+      dataType: 'json',
+      // 请求头
+      headers: { Authorization: localStorage.getItem('token') },
+      success(res) {
+        if (res.code === 200) {
+          // 利用模板引擎template来进行数据的渲染
+          let html = template('newListTemp', res)
+          $('#tbody').html(html)
+        } else {
+          console.log('获取失败')
+        }
+      }
+    })
+  }
+  getCategoryList()
+
+
+})
