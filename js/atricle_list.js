305b19b1544d2a8d0e4f9548959bed9d6b924ae7
fangYiTao <718891998@qq.com>
2021-08-14 17:58:49 +0800

    分页按钮功能实现完成

--- js/atricle_list.js
+++ js/atricle_list.js
@@ -2,11 +2,27 @@
 
 $(function () {
 
+  // 全局变量，请求参数
+  let params = {
+    //   key	string	搜索关键词，可以为空，为空返回某类型所有文章
+    key: '',
+    // type	string	文章类型id，可以为空，为空返回所有类型文章
+    type: '',
+    // state	string	文章状态，草稿 ，已发布,为空返回所有状态文章
+    state: '',
+    // page	number	当前页，为空返回第1页
+    page: '1',
+    // perpage	number	每页显示条数，为空默认每页6条
+    perpage: ''
+  }
+
+
   function articleQuery() {
     // 获取数据，发请求
     $.ajax({
       url: 'http://localhost:8080/api/v1/admin/article/query',
       headers: { Authorization: localStorage.getItem('token') },
+      data: params,
       success(res) {
         if (res.code === 200) {
           let html = template('atricleListTemp', res.data)
@@ -28,8 +44,21 @@ $(function () {
 
     //执行一个laypage实例
     laypage.render({
-      elem: 'pager' //注意，这里的 test1 是 ID，不用加 # 号
-      , count: totalCount //数据总数，从服务端得到
+      elem: 'pager', //注意，这里的 test1 是 ID，不用加 # 号
+      count: totalCount,//数据总数，从服务端得到
+      limit: 10,
+      curr: params.page,
+      jump: function (obj, first) {
+
+        //首次不执行
+        if (!first) {
+          //do something
+          // 获取当前点击的分页页码
+          params.page = obj.curr
+          // 重新发起异步请求
+          articleQuery()
+        }
+      }
     });
   }
 
