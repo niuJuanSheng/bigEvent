03a620078c3e78a893795229b949041d6e66591b
fangYiTao <718891998@qq.com>
2021-08-14 17:19:44 +0800

    动态渲染了分页组件

--- js/atricle_list.js
+++ js/atricle_list.js
@@ -11,6 +11,9 @@ $(function () {
         if (res.code === 200) {
           let html = template('atricleListTemp', res.data)
           $('.table tbody').html(html)
+          // 获取分页的总条数
+          const totalCount = res.data.totalCount
+          renerPager(totalCount)
         } else {
           console.log('获取失败')
         }
@@ -20,16 +23,16 @@ $(function () {
   }
   articleQuery()
 
-  function renerPager() {
+  function renerPager(totalCount) {
     var laypage = layui.laypage;
 
     //执行一个laypage实例
     laypage.render({
       elem: 'pager' //注意，这里的 test1 是 ID，不用加 # 号
-      , count: 50 //数据总数，从服务端得到
+      , count: totalCount //数据总数，从服务端得到
     });
   }
-  renerPager()
+
 
 
 
