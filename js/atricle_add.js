dd9a794bec3d2830da13e03c8cf3192511432252
fangYiTao <718891998@qq.com>
2021-08-15 15:42:41 +0800

    文章编辑组件的初始化

--- js/atricle_add.js
+++ js/atricle_add.js
@@ -1,27 +1,28 @@
+/* 
+1 组件的初始化
+  1 图片上传框 即时显示 （两个标签 图片标签 另外一个 文件上传标签）
+  2 动态渲染下拉列表-文章类别
+  3 初始化 layui的日期选择框
+  4 初始化 tinymce 富文本编辑器
 
-// 初始化日期插件
-function init_date() {
-  // 使用laydate模块
-  layui.use('laydate', function () {
-    var laydate = layui.laydate;
-    //执行一个laydate实例
-    laydate.render({
-      elem: '#articleDate'
-    });
-  });
-}
-
-// 初始化富文本插件
-function init_tinymce() {
-  tinymce.init({
-    selector: '#articleContent',
-    language: 'zh_CN'
-  });
-}
+*/
 
 $(function () {
 
-  // 获取文章分类
+  // 1. 图片上传框 即时显示
+  function uploadImg() {
+    $('#inputCover').on('change', function () {
+
+      const file = this.files[0]
+      $('#coverimg').prop('src', URL.createObjectURL(file))
+
+    })
+  }
+
+  // 1. 图片上传框 即时显示
+  uploadImg()
+
+  // 2. 动态渲染下拉列表-文章类别
   function getCategoryList() {
     $.ajax({
       url: 'http://localhost:8080/api/v1/admin/category/list',
@@ -32,10 +33,40 @@ $(function () {
       }
     })
   }
+
+  // 2. 动态渲染下拉列表-文章类别
   getCategoryList()
 
-  init_date()
-  init_tinymce()
+  // 3. 初始化 layui的日期选择框
+  function initDate() {
+
+    const laydate = layui.laydate;
+    //执行一个laydate实例
+    laydate.render({
+      elem: '#articleDate'
+
+    });
+  }
+
+  // 3. 初始化 layui的日期选择框
+  initDate()
+
+
+  // 4. 初始化 tinymce 富文本编辑器
+  function initTinymce() {
+    tinymce.init({
+      selector: '#articleContent',
+      language: 'zh_CN'
+    });
+  }
+
+  // 4. 初始化 tinymce 富文本编辑器
+  initTinymce()
+
+
+
+
 
 
 })
+
